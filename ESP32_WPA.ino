2006de9e15e7a1ce04cf50cdeab32634e2a4eb18
ETR <49214324+etorresr@users.noreply.github.com>
2023-03-15 12:27:05 -0600

    Update and rename esp32_firebase_tec_muestra_linux.ino to ESP32_WPA.ino

--- ESP32_WPA.ino
+++ ESP32_WPA.ino
@@ -17,21 +17,14 @@ const char* ssid = "Tec";
 #define EAP_IDENTITY "etorresr@tec.mx"
 #define EAP_PASSWORD "S....$"
 
-// Insert Firebase project API Key
-#define API_KEY "AIzaSyCNmj8I10tKFI1sAu_42nsggpnDa-WXM8A"//AIzaSyAjjTHMIV0y394tayvijhU-aVVcKdkIZxU
 
-// Insert RTDB URLefine the RTDB URL */
-#define DATABASE_URL "https://esp32-agosto-default-rtdb.firebaseio.com/"
+#define API_KEY "A---A"//AIzaSyAjjTHMIV0y394tayvijhU-aVVcKdkIZxU
+#define DATABASE_URL "https://esp32----rtdb.firebaseio.com/"
 
-//Define Firebase Data object
+
 FirebaseData fbdo;
-
 FirebaseAuth auth;
 FirebaseConfig config;
-
-unsigned long sendDataPrevMillis = 0;
-int intValue;
-float floatValue;
 
 bool signupOK = false;
 int count = 0;
@@ -47,17 +40,13 @@ void setup() {
     // WPA2 enterprise magic starts here
     WiFi.disconnect(true);
     WiFi.mode(WIFI_STA);   //init wifi mode
-  //esp_wifi_set_mac(ESP_IF_WIFI_STA, &masterCustomMac[0]);
-  Serial.print("MAC >> ");
-  Serial.println(WiFi.macAddress());
-  Serial.printf("Connecting to WiFi: %s ", ssid);
-  //esp_wifi_sta_wpa2_ent_set_ca_cert((uint8_t *)incommon_ca, strlen(incommon_ca) + 1);
-  esp_wifi_sta_wpa2_ent_set_identity((uint8_t *)EAP_IDENTITY, strlen(EAP_IDENTITY));
-  esp_wifi_sta_wpa2_ent_set_username((uint8_t *)EAP_IDENTITY, strlen(EAP_IDENTITY));
-  esp_wifi_sta_wpa2_ent_set_password((uint8_t *)EAP_PASSWORD, strlen(EAP_PASSWORD));
-  //esp_wpa2_config_t configW = WPA2_CONFIG_INIT_DEFAULT();
-  //esp_wifi_sta_wpa2_ent_enable(&configW);
-  esp_wifi_sta_wpa2_ent_enable();
+    Serial.print("MAC >> ");
+    Serial.println(WiFi.macAddress());
+    Serial.printf("Connecting to WiFi: %s ", ssid);
+    esp_wifi_sta_wpa2_ent_set_identity((uint8_t *)EAP_IDENTITY, strlen(EAP_IDENTITY));
+    esp_wifi_sta_wpa2_ent_set_username((uint8_t *)EAP_IDENTITY, strlen(EAP_IDENTITY));
+    esp_wifi_sta_wpa2_ent_set_password((uint8_t *)EAP_PASSWORD, strlen(EAP_PASSWORD));
+    esp_wifi_sta_wpa2_ent_enable();
     // WPA2 enterprise magic ends here
 
 
@@ -73,10 +62,7 @@ void setup() {
     Serial.println("IP address: ");
     Serial.println(WiFi.localIP());
 
-///* Assign the api key (required) */
   config.api_key = API_KEY;
-
-  /* Assign the RTDB URL (required) */
   config.database_url = DATABASE_URL;
 
   /* Sign up */
@@ -87,20 +73,11 @@ void setup() {
   else{
     Serial.printf("%s\n", config.signer.signupError.message.c_str());
   }
-
-  /* Assign the callback function for the long running token generation task */
-  config.token_status_callback = tokenStatusCallback; //see addons/TokenHelper.h
+  config.token_status_callback = tokenStatusCallback; 
 
   Firebase.begin(&config, &auth);
   Firebase.reconnectWiFi(true);
 }
-
-
-
-/*
- *  everyhting below this, in loop(), is just a standard request to a webserver and nothing else than an example to show that is works.
- *
- */
 
 int value = 0;
 
